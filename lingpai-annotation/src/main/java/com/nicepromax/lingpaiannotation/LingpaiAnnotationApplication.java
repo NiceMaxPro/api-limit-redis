f53cef1a356f13484eb4d7c793236719eb9e103e
NICEPROMAX <2251253534@qq.cpm>
2025-05-04 22:20:45 +0800

    令牌桶实现限流，不支持分布式系统

---
+++ lingpai-annotation/src/main/java/com/nicepromax/lingpaiannotation/LingpaiAnnotationApplication.java
@@ -0,0 +1,13 @@
+package com.nicepromax.lingpaiannotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LingpaiAnnotationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LingpaiAnnotationApplication.class, args);
+    }
+
+}
---
+++ lingpai-annotation/src/test/java/com/nicepromax/lingpaiannotation/LingpaiAnnotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicepromax.lingpaiannotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LingpaiAnnotationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

514fb4e43830d5ff4e2a90e11c2e8d06d4437b7e
NICEPROMAX <2251253534@qq.cpm>
2025-05-05 22:03:41 +0800

    Redis实现令牌桶实现限流，支持分布式系统，不支持接口限流，只支持整个系统的加注解的接口限流

---
+++ redis-lingpai-annotation/src/main/java/com/nicepromax/redislingpaiannotation/RedisLingpaiAnnotationApplication.java
@@ -0,0 +1,13 @@
+package com.nicepromax.redislingpaiannotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisLingpaiAnnotationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisLingpaiAnnotationApplication.class, args);
+	}
+
+}
---
+++ redis-lingpai-annotation/src/test/java/com/nicepromax/redislingpaiannotation/RedisLingpaiAnnotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicepromax.redislingpaiannotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisLingpaiAnnotationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
